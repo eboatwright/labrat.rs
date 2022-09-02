f96aac58a895967e64938e8457ef0bc7ab430356
eboatwright <64672325+eboatwright@users.noreply.github.com>
2022-09-02 16:49:28 -0500

    9/2/22 4:49 PM
     - added some comments
     - removed some unused code
     - added some checks
     - reversed path output to speed it up (now you have to do path.pop() to get the next position)

--- src/pathfinder.rs
+++ src/pathfinder.rs
@@ -4,12 +4,10 @@ pub struct Pathfinder {
 	pub start: (f32, f32),
 	pub end: (f32, f32),
 
+	// 0 is walkable, 1 is wall
 	pub world: Vec<Vec<usize>>,
-
 	pub nodes: Vec<Node>,
-
 	pub path: Vec<(f32, f32)>,
-	pub found_path: bool,
 
 	pub steps: u32,
 }
@@ -21,11 +19,8 @@ impl Pathfinder {
 			end,
 
 			world,
-
 			nodes: vec![],
-
 			path: vec![],
-			found_path: false,
 
 			steps: max_steps,
 		}
@@ -40,12 +35,8 @@ impl Pathfinder {
 				let neighbor_position = (x as f32, y as f32);
 				if neighbor_position == position
 				|| y >= self.world.len()
-				|| x >= self.world[0].len() {
-					continue;
-				}
-
-				if self.world[y][x] == 1
-				&& neighbor_position != self.end {
+				|| x >= self.world[0].len()
+				|| self.world[y][x] == 1 {
 					continue;
 				}
 
@@ -78,6 +69,11 @@ impl Pathfinder {
 	}
 
 	pub fn find_path(&mut self) {
+		// If the ending point is inside a wall, there's no point in looking for a way through it
+		if self.world[self.end.1 as usize][self.end.0 as usize] == 1 {
+			return;
+		}
+
 		let mut open: Vec<usize> = vec![];
 		let mut closed: Vec<usize> = vec![];
 
@@ -100,23 +96,29 @@ impl Pathfinder {
 			let mut current = usize::MAX;
 			for i in open.iter() {
 				if current == usize::MAX
-				|| self.nodes[*i].f() < self.nodes[current].f() {
+				|| self.nodes[*i].f() < self.nodes[current].f()
+				|| self.nodes[*i].f() == self.nodes[current].f()
+				&& self.nodes[*i].h < self.nodes[current].h {
 					current = *i;
 				}
 			}
 
-			open.retain(|x| *x != current);
+			for i in 0..open.len() {
+				if open[i] == current {
+					open.remove(i);
+					break;
+				}
+			}
 			closed.push(current);
 
 			if self.nodes[current].position == self.end {
-				self.found_path = true;
-
 				while self.nodes[current].position != self.start {
-					self.path.push(self.nodes[current].position);
 					current = self.nodes[current].from;
+					self.path.push(self.nodes[current].position);
 				}
 
-				self.path.reverse();
+				// Remove the starting position
+				self.path.pop();
 
 				return;
 			}
